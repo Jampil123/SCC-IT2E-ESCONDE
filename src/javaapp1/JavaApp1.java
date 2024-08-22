d7c28dbf0c0cb7505b8bd8fc4ce1651c93b7399d
Jampil123 <johnescondephil15@Gmail.com>
2024-08-23 00:19:07 +0800

    I added product class

--- src/javaapp1/JavaApp1.java
+++ src/javaapp1/JavaApp1.java
@@ -8,6 +8,18 @@ public class JavaApp1 {
         gr.addGrades(1011,"John Phil ",1.3,2.2,2.3,1.3);
         gr.viewGrades();
         
+        Grades gr1 = new Grades();
+        gr1.addGrades(1011,"John Phil ",3.3,3.2,3.3,3.3);
+        gr1.viewGrades();
+        
+//        Product pr = new Product();
+//        pr.addProducts(1011, "sabon", 30, 60, 0);
+//        pr.viewProducts();
+//        
+//        Product pr1 = new Product();
+//        pr1.addProducts(1011, "sabon", 30, 0, 0);
+//        pr1.viewProducts();
+        
     }
     
 }
