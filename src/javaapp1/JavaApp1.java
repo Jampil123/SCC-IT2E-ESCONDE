4e02b357672a70dba92986e223343893fe7707b9
Jampil123 <johnescondephil15@Gmail.com>
2024-08-22 22:43:39 +0800

    first upload

---
+++ src/javaapp1/JavaApp1.java
@@ -0,0 +1,13 @@
+
+package javaapp1;
+
+public class JavaApp1 {
+
+    public static void main(String[] args) {
+        Grades gr = new Grades();
+        gr.addGrades(1011,"John Phil ",1.3,2.2,2.3,1.3);
+        gr.viewGrades();
+        
+    }
+    
+}
